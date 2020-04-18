5eda2e5b2a403fa38287631de66c6f4a04e6bb05
soumya <soumyas567@gmail.com>
2020-04-18 22:17:18 +0100

    Added  error.html

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from flask import Flask, render_template, request
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html', )
+    return render_template('index.html')
 
 @app.route('/report.html')
 def report():
     filename= request.args.get('filename')
-    (total, j, k, l)= spellchecking(filename)
+    result= spellchecking(filename)
+    if result==False:
+        return render_template('error.html')
 
-    return(render_template('report.html',total=total,j=j,k=k,l=l))
+    return render_template('report.html', total=result[0], j=result[1], k=result[2], l=result[3])
 
 
 if __name__ == '__main__':
--- spellcheckfile.py
+++ spellcheckfile.py
@@ -1,24 +1,27 @@
-import string
 from spellchecker import SpellChecker
+import string
 
+spell = SpellChecker()
 def spellchecking(filename):
-    spell = SpellChecker()
+    spell =SpellChecker()
     total = lc = wc = 0
     i=0
-    lci=[]
-    wci=[]
-    wordi = []
-    with open(filename, "r") as f:
-        for line in f:
-            line = (line.translate(str.maketrans('', '', string.punctuation)))  # Remove punctuation from a string
-            lc += 1
-            wc=0
-            for word in line.split():
-                wc += 1
-                if not spell[word]:
-                    total = total + 1
-                    wordi.append(word)
-                    lci.append(lc)
-                    wci.append(wc)
-    return (total,wordi,lci,wci,)
-
+    linecountlist=[]
+    wordcountlist=[]
+    wordlist = []
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                line = (line.translate(str.maketrans('', '', string.punctuation)))  # Remove punctuation from a string
+                lc += 1
+                wc=0
+                for word in line.split():
+                    wc += 1
+                    if not spell[word]:
+                        total = total + 1
+                        wordlist.append(word)
+                        linecountlist.append(lc)
+                        wordcountlist.append(wc)
+        return (total,wordlist,linecountlist,wordcountlist)
+    except IOError:
+        return False
